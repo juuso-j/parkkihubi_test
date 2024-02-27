dbedeadd1376620c103c2f48c9fac55e7815bbd8
juuso-j <juuso.jokiniemi@turku.fi>
2024-02-27 09:48:04 +0200

    Format

--- server_tests/operator_api_tests.py
+++ server_tests/operator_api_tests.py
@@ -24,7 +24,7 @@ DATA = {
 
 
 def test_get_payment_zones():
-    response = requests.get(f'{PARKKI_HOST}/operator/v1/payment_zone/', headers=HEADERS)
+    response = requests.get(f"{PARKKI_HOST}/operator/v1/payment_zone/", headers=HEADERS)
     assert response.status_code == 200
     json_data = response.json()
     assert "count" in json_data
@@ -40,7 +40,7 @@ def test_get_payment_zones():
 
 
 def test_get_permit_areas():
-    response = requests.get(f'{PARKKI_HOST}/operator/v1/permit_area/', headers=HEADERS)
+    response = requests.get(f"{PARKKI_HOST}/operator/v1/permit_area/", headers=HEADERS)
     assert response.status_code == 200
     json_data = response.json()
     assert "count" in json_data
@@ -55,7 +55,7 @@ def test_get_permit_areas():
 
 def test_create_not_valid_parking_to_http(data=DATA):
     # Fails as posting to HTTP and not HTTPS
-    response = requests.post(f'{PARKKI_HTTP_HOST}/operator/v1/parking/', headers=HEADERS, json=data)
+    response = requests.post(f"{PARKKI_HTTP_HOST}/operator/v1/parking/", headers=HEADERS, json=data)
     assert response.status_code == 405
     assert "not allowed" in response.text
 
@@ -64,7 +64,7 @@ def test_create_not_valid_parking(data=DATA):
     # 'time_start' and 'time_end in the past
     data["time_start"] = (NOW - timedelta(hours=5)).strftime(TIMEFORMAT)
     data["time_end"] = (NOW - timedelta(hours=4)).strftime(TIMEFORMAT)
-    response = requests.post(f'{PARKKI_HOST}/operator/v1/parking/', headers=HEADERS, json=data)
+    response = requests.post(f"{PARKKI_HOST}/operator/v1/parking/", headers=HEADERS, json=data)
     assert response.status_code == 201
     json_data = response.json()
     assert json_data["status"] == "not_valid"
@@ -76,7 +76,7 @@ def test_create_not_valid_parking(data=DATA):
 def test_create_valid_parking(data=DATA):
     data["time_start"] = (NOW - timedelta(hours=4)).strftime(TIMEFORMAT)
     data["time_end"] = (NOW + timedelta(days=1, hours=1)).strftime(TIMEFORMAT)
-    response = requests.post(f'{PARKKI_HOST}/operator/v1/parking/', headers=HEADERS, json=data)
+    response = requests.post(f"{PARKKI_HOST}/operator/v1/parking/", headers=HEADERS, json=data)
     assert response.status_code == 201
     json_data = response.json()
     assert json_data["status"] == "valid"
@@ -86,13 +86,13 @@ def test_create_valid_parking(data=DATA):
 
 
 def test_delete_parking(id):
-    response = requests.delete(f'{PARKKI_HOST}/operator/v1/parking/{id}/', headers=HEADERS)
+    response = requests.delete(f"{PARKKI_HOST}/operator/v1/parking/{id}/", headers=HEADERS)
     assert response.status_code == 204
 
 
 def test_parking_replace_by_id_and_grace_period(id, data=DATA):
     data["registration_number"] = "TES-8"
-    response = requests.patch(f'{PARKKI_HOST}/operator/v1/parking/{id}/', headers=HEADERS, json=data)
+    response = requests.patch(f"{PARKKI_HOST}/operator/v1/parking/{id}/", headers=HEADERS, json=data)
     json_data = response.json()
     assert json_data["status"] == "valid"
     assert json_data["domain"] == TEST_DOMAIN
@@ -100,7 +100,7 @@ def test_parking_replace_by_id_and_grace_period(id, data=DATA):
     print("Waiting for 130seconds, to test that grace period(2 minutes) has passed....")
     data["registration_number"] = "TES-8"
     time.sleep(130)
-    response = requests.put(f'{PARKKI_HOST}/operator/v1/parking/{id}/', headers=HEADERS, json=data)
+    response = requests.put(f"{PARKKI_HOST}/operator/v1/parking/{id}/", headers=HEADERS, json=data)
     response.status_code == 403
     assert "Grace period has passed" in response.json()["detail"]
 
